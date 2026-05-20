f609aa3c345dd8ee37b086977c7078d6ba1986b4
Md Saiful Haque <saifulhaque.pciu@gmail.com>
2026-05-20 13:18:29 +0600

    created db

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,3 +3,38 @@ import config from "../config"
 
 
 export const sql = neon(config.database_url)
+
+export const initDB = async () => {
+    await sql`
+    CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(75) NOT NULL,
+    email VARCHAR(250) UNIQUE NOT NULL,
+
+    password TEXT NOT NULL,
+
+    age INT,
+    role VARCHAR(20) NOT NULL DEFAULT 'user',
+
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+    ) 
+    `
+
+    await sql`
+    CREATE TABLE IF NOT EXISTS orders(
+    id SERIAL PRIMARY KEY,
+    customer_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    quantity INT NOT NULL CHECK (quantity > 0),
+    food TEXT NOT NULL,
+    price NUMERIC(10, 2) NOT NULL,
+
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+    ) 
+    `
+
+
+
+    console.log("DB Connected");
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import app from "./app"
 import config from "./config";
+import { initDB } from "./db";
 
 
 const main = async () => {
+    initDB()
     app.listen(config.port, () => {
         console.log(`Server is running on port ${config.port}`);
     })
