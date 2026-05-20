38d398896b309546f154ff9de9408ddc13083dc9
Md Saiful Haque <saifulhaque.pciu@gmail.com>
2026-05-20 18:38:58 +0600

    generate access token

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,9 @@ dotenv.config({ quiet: true });
 const config = {
     port: env.PORT as string,
     database_url: env.DATABASE_URL as string,
-    node_env: env.NODE_ENV as string
+    node_env: env.NODE_ENV as string,
+    access_secret: env.ACCESS_SECRET as string,
+    refresh_secret: env.REFRESH_SECRET as string
 };
 
 export default config;
---
+++ src/utils/jwt.ts
@@ -0,0 +1,11 @@
+import config from "../config";
+import type { RUser } from "../types";
+import jwt from "jsonwebtoken"
+
+const signToken = (payload: RUser) => {
+    const accessToken = jwt.sign(payload, config.access_secret, { expiresIn: "1d" })
+
+    const refreshToken = jwt.sign(payload, config.refresh_secret, { expiresIn: "1d" })
+
+    return { accessToken, refreshToken }
+}
