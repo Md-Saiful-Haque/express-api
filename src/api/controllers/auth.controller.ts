b06b2aea986c2ccaf3bc88cf4d09fbd9ac0b475d
Md Saiful Haque <saifulhaque.pciu@gmail.com>
2026-05-21 12:32:46 +0600

    set refresh token

--- src/api/controllers/auth.controller.ts
+++ src/api/controllers/auth.controller.ts
@@ -37,19 +37,37 @@ export const login = async (req: Request, res: Response) => {
         refreshToken
     }
 
-    return sendResponse(res, { message: "User Login Successfully", data: result})
+    return sendResponse(res, { message: "User Login Successfully", data: result })
 
 }
 
 export const refresh = async (req: Request, res: Response) => {
     const refreshToken = req.cookies?.refreshToken
-    if(!refreshToken) {
-        return sendResponse(res, {message: "Refresh Token Not Found"}, 401)
+    if (!refreshToken) {
+        return sendResponse(res, { message: "Refresh Token Not Found" }, 401)
     }
 
     const payload = verifyToken(refreshToken, "refresh")
-    if(!payload) {
-        return sendResponse(res, {message: "Invalid Refresh Token"}, 401)
+    if (!payload) {
+        return sendResponse(res, { message: "Invalid Refresh Token" }, 401)
     }
-    console.log(payload);
+    // console.log(payload);
+
+    const user = await authService.getUserById(payload.id)
+     if (!user) {
+        return sendResponse(res, { message: "User Not Found" }, 404)
+    }
+
+    const {accessToken, refreshToken: newRefreshToken} = signToken(user)
+
+    res.cookie("refreshToken", newRefreshToken, {
+        secure: false,
+        sameSite: "lax",
+        httpOnly: true
+    })
+
+    sendResponse(res, { message: "Token Refreshed", data: {
+        accessToken,
+        newRefreshToken
+    }}, 404)
 }
--- src/api/service/auth.service.ts
+++ src/api/service/auth.service.ts
@@ -29,6 +29,13 @@ class AuthService {
 
         return isValid ? user : null
     }
+
+    async getUserById(id: string) {
+        const res = await sql`
+        SELECT id, name, email, age, role FROM users WHERE id = ${id}
+        `
+        return res[0] as RUser & {id: number}
+    }
 }
 
 export default new AuthService()
